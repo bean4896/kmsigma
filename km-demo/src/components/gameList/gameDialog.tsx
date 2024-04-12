c2383ef52e1ad657cce3396d477daa6b916038a3
KeYi <46948667+bean4896@users.noreply.github.com>
2024-04-12 18:11:12 +0800

    ADD DIALOG

--- km-demo/src/components/gameList/gameDialog.tsx
+++ km-demo/src/components/gameList/gameDialog.tsx
@@ -158,23 +158,25 @@ const GameDialog: React.FC<GameDialogProps> = ({ gameName, onClose }) => {
           Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
         </div>
 
-        <div className="mt-6 flex justify-end">
-          <button className="closeButton" onClick={onClose}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="#ffffff"
-              className="w-10 h-10"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-            </svg>
-          </button>
+        <div className="mt-6">
+          <div className="flex justify-end">
+            <button className="closeButton" onClick={onClose}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#ffffff"
+                className="w-10 h-10"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </div>
