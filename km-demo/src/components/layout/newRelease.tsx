20f992760060c33c1e6756948c5c8ed59781660c
KeYi <46948667+bean4896@users.noreply.github.com>
2024-04-19 15:55:08 +0800

    auto fuck

--- km-demo/src/components/layout/newRelease.tsx
+++ km-demo/src/components/layout/newRelease.tsx
@@ -5,6 +5,9 @@ import ImgLabel from "@/assets/newreleases_label.png";
 import Image from "next/image";
 import { Skeleton } from "@/components/ui/skeleton";
 import { EmblaOptionsType } from "embla-carousel";
+import GameSlidesinfo from "@/lib/gameSlides";
+import Autoplay from 'embla-carousel-autoplay'
+
 interface Game {
   id: number;
   gameName: string;
@@ -21,252 +24,10 @@ interface Game {
   };
   isFeatured: boolean;
 }
-const GameSlidesinfo = [
-  {
-    id: 1,
-    gameName: "Penguin Panic",
-    tags: ["tag1", "tag2"],
-    backgroundUrl:
-      "https://res.cloudinary.com/detatjujs/image/upload/v1713511412/ns-penguin_vnovdw.png",
-    category: "NEXT-GEN",
-    thumbnail: "thumbnail url",
-    langProps: {
-      english: {
-        gameName: "Penguin Panic",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      spanish: {
-        gameName: "Panic de Pingüinos",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      brazil: {
-        gameName: "Pânico dos Pinguins",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-    },
-    isFeatured: true,
-  },
-  {
-    id: 2,
-    gameName: "Jackpot Jump",
-    tags: ["tag1", "tag2"],
-    backgroundUrl:
-      "https://res.cloudinary.com/detatjujs/image/upload/v1713511411/ns-jackpotjump_xmvgzt.png",
-    category: "NEXT-GEN",
-    thumbnail: "thumbnail url",
-    langProps: {
-      english: {
-        gameName: "Jackpot Jump",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      spanish: {
-        gameName: "Panic de Pingüinos",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      brazil: {
-        gameName: "Pânico dos Pinguins",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-    },
-    isFeatured: true,
-  },
-  {
-    id: 3,
-    gameName: "Interstellar RUN",
-    tags: ["tag1", "tag2"],
-    backgroundUrl:
-      "https://res.cloudinary.com/detatjujs/image/upload/v1713511409/ns-interstellar_h7gx3j.png",
-    category: "NEXT-GEN",
-    thumbnail: "thumbnail url",
-    langProps: {
-      english: {
-        gameName: "Interstellar RUN",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      spanish: {
-        gameName: "Panic de Pingüinos",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      brazil: {
-        gameName: "Pânico dos Pinguins",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-    },
-    isFeatured: true,
-  },
-  {
-    id: 4,
-    gameName: "Sugar Blast",
-    tags: ["tag1", "tag2"],
-    backgroundUrl:
-      "https://res.cloudinary.com/detatjujs/image/upload/v1713511415/ns-sugar_vf75ft.png",
-    category: "SLOTS",
-    thumbnail: "thumbnail url",
-    langProps: {
-      english: {
-        gameName: "Sugar Blast",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      spanish: {
-        gameName: "Panic de Pingüinos",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      brazil: {
-        gameName: "Pânico dos Pinguins",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-    },
-    isFeatured: true,
-  },
-  {
-    id: 5,
-    gameName: "Rooster Blitz",
-    tags: ["tag1", "tag2"],
-    backgroundUrl:
-      "https://res.cloudinary.com/detatjujs/image/upload/v1713511414/ns-rooster_gd29vg.png",
-    category: "SLOTS",
-    thumbnail: "thumbnail url",
-    langProps: {
-      english: {
-        gameName: "Rooster Blitz",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      spanish: {
-        gameName: "Panic de Pingüinos",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      brazil: {
-        gameName: "Pânico dos Pinguins",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-    },
-    isFeatured: true,
-  },
-  {
-    id: 6,
-    gameName: "Cleopatras Treasure",
-    tags: ["tag1", "tag2"],
-    backgroundUrl:
-      "https://res.cloudinary.com/detatjujs/image/upload/v1713511409/ns-cleo_dea5rc.png",
-    category: "SLOTS",
-    thumbnail: "thumbnail url",
-    langProps: {
-      english: {
-        gameName: "Cleopatras Treasure",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      spanish: {
-        gameName: "Panic de Pingüinos",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      brazil: {
-        gameName: "Pânico dos Pinguins",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-    },
-    isFeatured: true,
-  },
-  {
-    id: 7,
-    gameName: "Baccarat",
-    tags: ["tag1", "tag2"],
-    backgroundUrl:
-      "https://res.cloudinary.com/detatjujs/image/upload/v1713511409/ns-baccarat_w1zqsc.png",
-    category: "CLASSICS",
-    thumbnail: "thumbnail url",
-    langProps: {
-      english: {
-        gameName: "Baccarat",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      spanish: {
-        gameName: "Panic de Pingüinos",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      brazil: {
-        gameName: "Pânico dos Pinguins",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-    },
-    isFeatured: true,
-  },
-  {
-    id: 8,
-    gameName: "KM Power Ball",
-    tags: ["tag1", "tag2"],
-    backgroundUrl:
-      "https://res.cloudinary.com/detatjujs/image/upload/v1713511410/ns-kmball_ktscxc.png",
-    category: "CLASSICS",
-    thumbnail: "thumbnail url",
-    langProps: {
-      english: {
-        gameName: "KM Power Ball",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      spanish: {
-        gameName: "Panic de Pingüinos",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      brazil: {
-        gameName: "Pânico dos Pinguins",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-    },
-    isFeatured: true,
-  },
-  {
-    id: 9,
-    gameName: "KM Virtual Horse Racing",
-    tags: ["tag1", "tag2"],
-    backgroundUrl:
-      "https://res.cloudinary.com/detatjujs/image/upload/v1713511410/ns-horse_zlz29w.png",
-    category: "CLASSICS",
-    thumbnail: "thumbnail url",
-    langProps: {
-      english: {
-        gameName: "KM Virtual Horse Racing",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      spanish: {
-        gameName: "Panic de Pingüinos",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-      brazil: {
-        gameName: "Pânico dos Pinguins",
-        iframeUrl: "iframe url",
-        gameUrl: "game url",
-      },
-    },
-    isFeatured: true,
-  },
-];
-const OPTIONS: EmblaOptionsType = {};
+
+const OPTIONS: EmblaOptionsType = {
+  loop: true, // Enable looping
+};
 
 type NewReleaseProps = {
   selectedCategory: string;
@@ -275,7 +36,9 @@ type NewReleaseProps = {
 const NewRelease = ({ selectedCategory }: NewReleaseProps) => {
   const [isLoading, setIsLoading] = useState(true);
   const [filteredSlides, setFilteredSlides] = useState<number[]>([]);
-
+  const [emblaRef, emblaApi] = useEmblaCarousel(OPTIONS, [
+    Autoplay({ playOnInit: true, delay: 3000 })
+  ])
   useEffect(() => {
     setIsLoading(true);
 
@@ -285,13 +48,12 @@ const NewRelease = ({ selectedCategory }: NewReleaseProps) => {
     );
     setFilteredSlides(filteredGames.map((game: Game) => game.id));
 
-    // Simulating loading delay of 2 seconds
     setTimeout(() => {
       setIsLoading(false);
     }, 200);
-    console.log("filteredSlides", filteredSlides);
-    console.log("selectedCategory", selectedCategory);
-    console.log("filteredGames", filteredGames);
+    // console.log("filteredSlides", filteredSlides);
+    // console.log("selectedCategory", selectedCategory);
+    // console.log("filteredGames", filteredGames);
   }, [selectedCategory]);
 
   return (
---
+++ km-demo/src/lib/gameSlides.tsx
@@ -0,0 +1,247 @@
+const GameSlidesinfo = [
+    {
+      id: 1,
+      gameName: "Penguin Panic",
+      tags: ["tag1", "tag2"],
+      backgroundUrl:
+        "https://res.cloudinary.com/detatjujs/image/upload/v1713511412/ns-penguin_vnovdw.png",
+      category: "NEXT-GEN",
+      thumbnail: "thumbnail url",
+      langProps: {
+        english: {
+          gameName: "Penguin Panic",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        spanish: {
+          gameName: "Panic de Pingüinos",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        brazil: {
+          gameName: "Pânico dos Pinguins",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+      },
+      isFeatured: true,
+    },
+    {
+      id: 2,
+      gameName: "Jackpot Jump",
+      tags: ["tag1", "tag2"],
+      backgroundUrl:
+        "https://res.cloudinary.com/detatjujs/image/upload/v1713511411/ns-jackpotjump_xmvgzt.png",
+      category: "NEXT-GEN",
+      thumbnail: "thumbnail url",
+      langProps: {
+        english: {
+          gameName: "Jackpot Jump",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        spanish: {
+          gameName: "Panic de Pingüinos",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        brazil: {
+          gameName: "Pânico dos Pinguins",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+      },
+      isFeatured: true,
+    },
+    {
+      id: 3,
+      gameName: "Interstellar RUN",
+      tags: ["tag1", "tag2"],
+      backgroundUrl:
+        "https://res.cloudinary.com/detatjujs/image/upload/v1713511409/ns-interstellar_h7gx3j.png",
+      category: "NEXT-GEN",
+      thumbnail: "thumbnail url",
+      langProps: {
+        english: {
+          gameName: "Interstellar RUN",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        spanish: {
+          gameName: "Panic de Pingüinos",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        brazil: {
+          gameName: "Pânico dos Pinguins",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+      },
+      isFeatured: true,
+    },
+    {
+      id: 4,
+      gameName: "Sugar Blast",
+      tags: ["tag1", "tag2"],
+      backgroundUrl:
+        "https://res.cloudinary.com/detatjujs/image/upload/v1713511415/ns-sugar_vf75ft.png",
+      category: "SLOTS",
+      thumbnail: "thumbnail url",
+      langProps: {
+        english: {
+          gameName: "Sugar Blast",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        spanish: {
+          gameName: "Panic de Pingüinos",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        brazil: {
+          gameName: "Pânico dos Pinguins",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+      },
+      isFeatured: true,
+    },
+    {
+      id: 5,
+      gameName: "Rooster Blitz",
+      tags: ["tag1", "tag2"],
+      backgroundUrl:
+        "https://res.cloudinary.com/detatjujs/image/upload/v1713511414/ns-rooster_gd29vg.png",
+      category: "SLOTS",
+      thumbnail: "thumbnail url",
+      langProps: {
+        english: {
+          gameName: "Rooster Blitz",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        spanish: {
+          gameName: "Panic de Pingüinos",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        brazil: {
+          gameName: "Pânico dos Pinguins",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+      },
+      isFeatured: true,
+    },
+    {
+      id: 6,
+      gameName: "Cleopatras Treasure",
+      tags: ["tag1", "tag2"],
+      backgroundUrl:
+        "https://res.cloudinary.com/detatjujs/image/upload/v1713511409/ns-cleo_dea5rc.png",
+      category: "SLOTS",
+      thumbnail: "thumbnail url",
+      langProps: {
+        english: {
+          gameName: "Cleopatras Treasure",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        spanish: {
+          gameName: "Panic de Pingüinos",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        brazil: {
+          gameName: "Pânico dos Pinguins",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+      },
+      isFeatured: true,
+    },
+    {
+      id: 7,
+      gameName: "Baccarat",
+      tags: ["tag1", "tag2"],
+      backgroundUrl:
+        "https://res.cloudinary.com/detatjujs/image/upload/v1713511409/ns-baccarat_w1zqsc.png",
+      category: "CLASSICS",
+      thumbnail: "thumbnail url",
+      langProps: {
+        english: {
+          gameName: "Baccarat",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        spanish: {
+          gameName: "Panic de Pingüinos",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        brazil: {
+          gameName: "Pânico dos Pinguins",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+      },
+      isFeatured: true,
+    },
+    {
+      id: 8,
+      gameName: "KM Power Ball",
+      tags: ["tag1", "tag2"],
+      backgroundUrl:
+        "https://res.cloudinary.com/detatjujs/image/upload/v1713511410/ns-kmball_ktscxc.png",
+      category: "CLASSICS",
+      thumbnail: "thumbnail url",
+      langProps: {
+        english: {
+          gameName: "KM Power Ball",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        spanish: {
+          gameName: "Panic de Pingüinos",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        brazil: {
+          gameName: "Pânico dos Pinguins",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+      },
+      isFeatured: true,
+    },
+    {
+      id: 9,
+      gameName: "KM Virtual Horse Racing",
+      tags: ["tag1", "tag2"],
+      backgroundUrl:
+        "https://res.cloudinary.com/detatjujs/image/upload/v1713511410/ns-horse_zlz29w.png",
+      category: "CLASSICS",
+      thumbnail: "thumbnail url",
+      langProps: {
+        english: {
+          gameName: "KM Virtual Horse Racing",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        spanish: {
+          gameName: "Panic de Pingüinos",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+        brazil: {
+          gameName: "Pânico dos Pinguins",
+          iframeUrl: "iframe url",
+          gameUrl: "game url",
+        },
+      },
+      isFeatured: true,
+    },
+  ];
+
+  export default GameSlidesinfo;
