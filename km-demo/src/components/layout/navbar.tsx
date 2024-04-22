2ba8ee619f7ded6efb2b18565eb8933a3b3981bf
Ittachi <dkybone1996@gmail.com>
2024-04-23 07:37:52 +0800

    wtf

--- km-demo/src/components/layout/navbar.tsx
+++ km-demo/src/components/layout/navbar.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <div>
-      <nav className="fixed overflow-hidden top-0 h-[3.5%] w-full bg-black bg-opacity-70 flex justify-center items-center z-50">
+      <nav className="fixed top-0 w-full bg-black bg-opacity-70 flex justify-center items-center z-50">
         <div className="flex justify-center items-center">
           <Image
             src="https://res.cloudinary.com/detatjujs/image/upload/v1712826702/kingmidas-logo-gold_2k_d7maa0.png"
             alt="Logo"
             width={1000}
             height={1000}
-            className="h-10 w-auto"
+            className="w-auto h-[8rem]"
           />
         </div>
       </nav>
