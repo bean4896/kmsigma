bda2b9e1c9001273c6307bc5d2ee24c21f9f55de
bean4896 <dkybone1996@gmail.com>
2025-12-02 15:54:55 +0800

    remove info on top

--- km-demo/src/components/layout/qrcodeBanner.tsx
+++ km-demo/src/components/layout/qrcodeBanner.tsx
@@ -1,19 +1,7 @@
 'use client';
-import ImgQrcode from "@/assets/demosite-qr-km-website.jpg";
-import { useLanguage } from "@/context/LanguageContext";
-import { translations } from "@/lib/constent";
 import React from "react";
 
 const Banner = () => {
-  const { selectedLanguage } = useLanguage();
-
-  // Get the translated top banner message based on the selected language
-  const topBannerMessage =
-    translations.topBannerMessage[selectedLanguage as keyof typeof translations.topBannerMessage] ||
-    translations.topBannerMessage.English;
-
-  // Split the message by newline characters to retain <br /> for line breaks
-  const bannerLines = topBannerMessage.split("\n");
 
   return (
     <div
@@ -34,33 +22,11 @@ const Banner = () => {
         }}
       ></div>
 
-      {/* Content */}
-      <div className="qrcontainer flex items-center ml-10 p-4 relative" style={{ zIndex: 2 }}>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-          <div className="flex items-center mt-[100px] lg:mt-[0px]">
-            <h3 className="text-white text-left title-banner leading-snug">
-              {bannerLines.map((line, index) => (
-                <React.Fragment key={index}>
-                  {line}
-                  <br />
-                </React.Fragment>
-              ))}
-            </h3>
-          </div>
-          <div className="flex items-center">
-            <img
-              src={ImgQrcode.src} // Resolved path for the image
-              alt="QR Code"
-              className="qrcodeImg"
-              style={{
-                width: "100%", // Adjust styles as needed
-                height: "auto",
-                maxWidth: "200px", // Optional: Limit maximum width
-              }}
-            />
-          </div>
-        </div>
-      </div>
+      {/* Empty content area (QR code and text removed) */}
+      <div
+        className="qrcontainer relative"
+        style={{ zIndex: 2 }}
+      ></div>
     </div>
   );
 };
