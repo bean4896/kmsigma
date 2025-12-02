a22929d95638aaa669f2bdc1f99be78206e0f952
bean4896 <dkybone1996@gmail.com>
2025-12-02 15:49:50 +0800

    add links bottom

--- km-demo/src/app/page.tsx
+++ km-demo/src/app/page.tsx
@@ -5,8 +5,8 @@ import QrCode from "@/components/layout/qrcode";
 import Banner from "@/components/layout/qrcodeBanner";
 import NewRelease from "@/components/layout/newRelease";
 import Image from "next/image";
-
-
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const Home: React.FC = () => {
   // Get unique categories from game data
@@ -18,6 +18,35 @@ const Home: React.FC = () => {
       <main className="flex min-h-screen flex-col max-w-[100vw] m-auto">
         <GameListWithFilter games={GameData} categories={categories} />
         <QrCode />
+
+        <div className="mt-auto flex flex-col items-center gap-4 my-6">
+          <Button
+            asChild
+            className="px-8 bg-white text-black hover:bg-white/90"
+          >
+            <a
+              href="https://kingmidasgames.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Discover more at KingMidas Games
+            </a>
+          </Button>
+
+          <Button
+            asChild
+            variant="outline"
+            className="px-8 bg-white text-black hover:bg-white/90"
+          >
+            <a
+              href="https://www.jotform.com/242482741492460"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Speak to us today
+            </a>
+          </Button>
+        </div>
       </main>
       {/* <Image
         id="overlay-image"
